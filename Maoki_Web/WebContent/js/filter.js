b528aa32fe8fc04b419bf4529c3e2bd173cb6a60
Higiri <higiri4869@gmail.com>
2020-08-29 18:01:49 +0900

    [fix] デザインの大幅変更

--- Maoki_Web/WebContent/js/filter.js
+++ Maoki_Web/WebContent/js/filter.js
@@ -1,10 +1,24 @@
 /*
  *
  */
+
 $(function () {
     $('.filter').filterizr();
     var filterSingle = $('.filter').filterizr({
-        layout: 'sameWidth'
+        layout: 'sameWidth',
+        
+        // +
+        spinner: { // Configuration for built-in spinner
+            enabled: false,
+            fillColor: '#2184D0',
+            styles: {
+              height: '75px',
+              margin: '0 auto',
+              width: '75px',
+              'z-index': 2,
+            },
+          },
+        // +
     });
 });
 
@@ -17,3 +31,7 @@ $(document).on('click', '.sort', function () {
 	  $('.sort').removeClass('active');
 	  $(this).addClass('active');
 	});
+
+
+
+
